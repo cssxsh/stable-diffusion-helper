45453a390fc3a5fb4176019098ea1ad39134c65b
DRSalieri <57832639+DRSalieri@users.noreply.github.com>
2023-03-08 19:42:10 +0800

    Update src/main/kotlin/xyz/cssxsh/mirai/diffusion/StableDiffusionListener.kt
    Co-authored-by: cssxsh <cssxsh@gmail.com>

--- src/main/kotlin/xyz/cssxsh/mirai/diffusion/StableDiffusionListener.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/diffusion/StableDiffusionListener.kt
@@ -176,6 +176,7 @@ public object StableDiffusionListener : SimpleListenerHost() {
                     subject.uploadImage(temp)
                 }.toMessageChain()
 
+                }
                 subject.sendMessage(message)
             }
         }
